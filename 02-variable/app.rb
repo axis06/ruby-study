98a7dfdecc25010180af93c9f18df64eb0ef5380
axis06 <withcomputer@gmail.com>
2016-06-25 11:45:48 +0900

    variable

---
+++ 02-variable/app.rb
@@ -0,0 +1,35 @@
+fruit = "apple" #変数
+puts fruit
+
+FRUIT = "orange" #定数
+puts FRUIT
+
+#######################################
+
+_filename = 'text.txt' #ローカル変数
+$flag = true #フローバル変数
+
+puts _filename
+puts $flag
+
+#######################################
+
+#@age = 18 #インスタンス変数
+#@@count = 1 #クラス変数
+
+#######################################
+
+a = fruit
+puts a
+
+fruit << " MacBookAir"
+puts a
+
+#######################################
+
+foo = 10
+foo += 1
+puts foo
+
+c,d,n = 2,3,4
+puts c == 2
